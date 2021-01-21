607ac46843a726f2b2472802a49a66a11a33513c
Camilo Vasquez <camilovasquez9738@hotmail.com>
2021-01-21 18:00:26 -0500

    substract and sum methods with an interface running

--- Calculator/src/Test/Runner.java
+++ Calculator/src/Test/Runner.java
@@ -1,8 +1,50 @@
 package Test;
 
+import Data.Calculator;
+import java.util.Scanner;
+
 public class Runner {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        String enter = "y";
+        while(enter.equals("y")) {
+
+            System.out.println("JAVA CALCULATOR \n" +
+                    "\n" +
+                    "Sum (3)\n" +
+                    "Substract (4)");
+            int operation = scanner.nextInt();
+
+            switch (operation) {
+
+                case 3:
+                    System.out.println("What is the first number?");
+                    int numToSum1 = scanner.nextInt();
+                    System.out.println("What is the second number?");
+                    int numToSum2 = scanner.nextInt();
+                    System.out.println("The result of the sum is " + calculator.sumMethod(numToSum1, numToSum2));
+                    break;
+                case 4:
+                    System.out.println("What is the first number?");
+                    int numToSubst1 = scanner.nextInt();
+                    System.out.println("What is the second number?");
+                    int numToSubst2 = scanner.nextInt();
+                    System.out.println("The result of the substraction is " + calculator.substractMethod(numToSubst1, numToSubst2));
+                    break;
+                default:
+                    System.out.println("That's an incorrect option");
+                    break;
+
+            }
+
+            System.out.println("Do you want to do another operation?\n" +
+                    "yes (y), no (n)");
+            enter = scanner.next();
+
+        }
 
     }
 }
